11e170df25d6e4fee25a1d55c092a127b700fce9
Danil Pismenny <danil@orionet.ru>
2015-03-17 15:09:14 +0400

    Add ./prototype

---
+++ prototype/w1/order_delivery_service.rb
@@ -0,0 +1,124 @@
+# encoding: utf-8
+require 'cgi'
+module W1
+  class OrderDeliveryService
+    def initialize order
+      @order = order
+    end
+
+    def create
+      debug message: "OrderDeliveryService#create", fields: fields.to_s
+
+      response
+
+      info message: "Delivery order", 
+        url: uri.to_s, 
+        response_code: response.code, 
+        response_body: response.body, 
+        fields: fields.to_s
+
+      raise W1::OrderDeliveryService::Error.new response unless response.code.to_i == 302
+
+      # Раньше подтверждали только после удачи,
+      # теперь сразу
+      order.order_delivery.delivery!
+
+      redirect_uri = pull_redirect_uri response.body
+      order.order_payment.update_attributes! external_id: pull_w1_order_id( redirect_uri )
+
+      debug message: "Делаю подтверждающий редирект", url: redirect_uri.to_s
+
+      get_response = Net::HTTP.get_response redirect_uri
+
+      if get_response.code.to_i == 302
+        debug message: "Подтверждающий редирект - DONE", 
+          url: redirect_uri.to_s, 
+          response_body: response.body
+
+        # Раньше подтверждали только после удачи
+        #order.order_delivery.delivery!
+      else
+        error message: "Ошибка подтверждающего редиректа", 
+          url: redirect_uri.to_s, 
+          response_body: response.body, 
+          response_code: response.code
+      end
+
+
+      return true
+
+      # в 20-й строке!
+      # EOFError: end of file reached>
+      # "302"
+      # "<html><head><title>Object moved</title></head><body>\r\n<h2>Object moved to <a href=\"https://www.walletone.com/checkout/Default.aspx?i=347508545860&amp;m=127696943127\">here</a>.</h2>\r\n</body></html>\r\n"
+    rescue => err
+      raise err if Rails.env.test?
+      raise err if defined?( VCR ) && err.is_a?( VCR::Errors::UnhandledHTTPRequestError )
+      binding.pry if Rails.env.development?
+      error message: "Delivery order", error: err.inspect
+      Honeybadger.notify err, context: { order_id: order.id }
+      return false
+    end
+
+    private
+
+    attr_reader :order
+
+    #  <html><head><title>Object moved</title></head><body>
+    #  <h2>Object moved to <a href="https://www.walletone.com/checkout/Default.aspx?i=347632112961&amp;m=127696943127">here</a>.</h2>;
+    #  </body></html>
+
+    def pull_redirect_uri body
+      Addressable::URI.parse Nokogiri::HTML(body).css('body/h2/a').attr('href').value
+    rescue
+      nil
+    end
+
+    def error params
+      W1.logger.error params.merge order_id: order.id
+    end
+
+    def info params
+      W1.logger.info params.merge order_id: order.id
+    end
+
+    def debug params
+      W1.logger.debug params.merge order_id: order.id
+    end
+
+    def response
+      @response ||= Net::HTTP.post_form uri, fields
+    end
+
+    def pull_w1_order_id uri
+      # "https://www.walletone.com/checkout/Default.aspx?i=347632097524&m=127696943127"
+      return CGI.parse(uri.query)['i'].first
+    rescue => err
+      raise err if Rails.env.test?
+      err.to_s
+    end
+
+    def uri
+      @url ||= URI.parse API_CHECKOUT_URL
+    end
+
+    def fields
+      @fields ||= FormOptions.generate order
+    end
+
+  end
+end
+
+class W1::OrderDeliveryService::Error < StandardError
+  def initialize response
+    @response = response
+  end
+  def message
+    "Ошибка отправки запроса: #{@response.code.to_i}"
+  end
+
+  def inspect
+    "#{@response.code} #{@response.body}"
+  end
+end
+
---
+++ prototype/w1/registration_service.rb
@@ -0,0 +1,29 @@
+# encoding: utf-8
+class W1::RegistrationService
+  include W1::Requester
+  ENDPOINT = 'registration/users'
+
+  def initialize vendor
+    @vendor = vendor
+    @vendor_form = W1::RegistrationForm.build_from_object(@vendor)
+  end
+
+  def register
+    response = post(ENDPOINT, @vendor_form)
+    response_json = parse_response response
+
+    case true
+    when response_json.blank? || response.code.to_i == 404
+      W1.error "Ошибка регистрации мерчанта [vendor_id=#{@vendor.id}]"
+      raise VendorRegistrationError::W1Error.new(@vendor)
+    when response_json['IsAlreadyRegistered'] == true
+      W1.log "Дубликат мерчанта [vendor_id=#{@vendor.id}]"
+      raise VendorRegistrationError::W1DuplicateMerchant.new(@vendor)
+    when response.code.to_i == 201
+      if @vendor.update_attributes! w1_merchant_id: response_json['UserId']
+        W1.log "Мерчант зарегистрирован [vendor_id=#{@vendor.id}]"
+      end
+    end
+    return true
+  end
+end
