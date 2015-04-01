19e7a9e310350af4ba068f42a0e2b569cc2e9de3
Danil Pismenny <email>
2015-04-01 16:02:36 +0000

    [#2] Walletone.logger

--- lib/walletone.rb
+++ lib/walletone.rb
@@ -1,3 +1,5 @@
+require 'logger'
+
 require 'walletone/errors'
 require 'walletone/form'
 require 'walletone/response_validator'
@@ -6,4 +8,7 @@ module Walletone
   API_URL          = 'https://api.w1.ru/OpenApi/'
   WEB_CHECKOUT_URL = 'https://wl.walletone.com/checkout/checkout/Index'
   API_CHECKOUT_URL = 'https://www.walletone.com/checkout/default.aspx'
+
+  mattr_accessor :logger
+  self.logger = Logger.new(STDERR)
 end
