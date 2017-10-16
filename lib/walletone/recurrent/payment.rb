c3ca7d2b164d1c067a101a098cbcbae742346c06
Danil Pismenny <danil@brandymint.ru>
2017-10-16 14:20:43 +0300

    Добавил WMI_ORDER_ITEMS

--- lib/walletone/recurrent/payment.rb
+++ lib/walletone/recurrent/payment.rb
@@ -1,5 +1,5 @@
 class Walletone::Recurrent::Payment < Walletone::Payment
-  define_fields %i(WMI_CUSTOMER_ID WMI_RECURRING_AGREEMENT_URL WMI_CUSTOMER_PHONE)
+  define_fields %i(WMI_CUSTOMER_ID WMI_RECURRING_AGREEMENT_URL)
 
   def valid?
     super && self.WMI_CUSTOMER_ID && self.WMI_RECURRING_AGREEMENT_URL
